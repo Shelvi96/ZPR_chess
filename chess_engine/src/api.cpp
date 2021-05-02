a7487bb7110199dd4d8c8238bf7ad26bc8134215
Konrad Owsikowski <konrad.owsikowski@gmail.com>
2021-05-03 00:21:51 +0200

    Board - minor style fix. Added game over info.

--- chess_engine/src/api.cpp
+++ chess_engine/src/api.cpp
@@ -1,10 +1,12 @@
 #include "api.h"
 #include "Board.h"
+#include "Piece.h"
 #include "MoveGenerator.h"
 
 std::string getNextFen(std::string fenString, int i_old, int j_old, int i_new, int j_new) {
     Board b = Board(fenString);
     MoveGenerator m = MoveGenerator(b);
+    int legalMoves = 0;
     int from = i_old * b.getWidth() + j_old;
     int to = i_new * b.getWidth() + j_new;
 
@@ -12,6 +14,7 @@ std::string getNextFen(std::string fenString, int i_old, int j_old, int i_new, i
         if (move.getFrom() == from && move.getTo() == to) {
             Board temp = m.MakeAMove(b, move);
             if(m.isMoveLegal(temp)) {
+                ++legalMoves;
                 if(move.isItCastling()) {
                     Board temp = m.MakeCastlingMove(b, move);
                     return temp.getFenString();
@@ -21,5 +24,7 @@ std::string getNextFen(std::string fenString, int i_old, int j_old, int i_new, i
             }
         }
     }
+    if (legalMoves == 0)
+        return (b.getActiveColor() == Color::white ? "GAME OVER : Black wins!" : "GAME OVER  : White wins!");
     return "";
 }
