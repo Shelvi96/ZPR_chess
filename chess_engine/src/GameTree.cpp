a214f4ced158890bc5d0c1c308637a0581a5d85f
kubajurasz <kubajurasz@gmail.com>
2021-05-29 22:01:06 +0200

    added alpha beta algorithm

--- chess_engine/src/GameTree.cpp
+++ chess_engine/src/GameTree.cpp
@@ -14,6 +14,8 @@ Board &Node::getBoard() {
     return board_;
 }
 
+
+
 void Node::generateChildren(int maxDepth) {
 
     moveGenerator_ = MoveGenerator(board_);
@@ -35,6 +37,14 @@ int Node::numberOfChildren() {
 
 std::vector<Node*> &Node::getChildren() {
     return children_;
+}
+
+void Node::generateMoves() {
+    moveGenerator_ = MoveGenerator(board_);
+}
+
+MoveGenerator Node::getMoveGenerator() {
+    return moveGenerator_;
 }
 
 
@@ -71,6 +81,98 @@ int GameTree::countPossibleBoards(Node *node, int level) {
             counter = counter + countPossibleBoards(child, level - 1);
         }
         return counter;
+    }
+}
+
+std::string GameTree::getBestMove(int depth, bool maximizingPlayer) {
+    generateTree(1);
+    std::string ret;
+    if(maximizingPlayer) {
+        float maxEval = -INFINITY;
+        int best_move = 0;
+        float alpha = -INFINITY;
+        float beta = INFINITY;
+        for(int i = 0; i < root->getChildren().size(); ++i) {
+            float eval = alphaBeta(root->getChildren()[i], depth - 1, alpha, beta, false);
+            if(eval >= maxEval) {
+                best_move = i;
+                maxEval = eval;
+            }
+
+        }
+        ret = root->getChildren()[best_move]->getBoard().getFenString();
+    } else {
+        float minEval = INFINITY;
+        int best_move = 0;
+        float alpha = -INFINITY;
+        float beta = INFINITY;
+        for(int i = 0; i < root->getChildren().size(); ++i) {
+            float eval = alphaBeta(root->getChildren()[i], depth - 1, alpha, beta, true);
+            if(eval < minEval){
+                best_move = i;
+                minEval = eval;
+
+            }
+            beta = std::min(minEval, beta);
+            std::cout << alpha << " " << beta << " " << eval << " " << best_move << '\n';
+            if(beta <= alpha)
+                break;
+        }
+        ret = root->getChildren()[best_move]->getBoard().getFenString();
+    }
+
+
+
+    deleteTree(root);
+    return ret;
+}
+
+
+float GameTree::alphaBeta(Node *node, int depth, float alpha, float beta, bool maximizingPlayer) {
+    if(depth == 0)
+        return node->getBoard().eval();
+
+    node->generateMoves();
+
+    if(maximizingPlayer) {
+        Node *temp;
+        float maxEval = -INFINITY;
+        for(auto &move : node->getMoveGenerator().getMoves()) {
+            if(move.isItCastling())
+                temp = new Node(node->getMoveGenerator().MakeCastlingMove(node->getBoard(), move), 0);
+            else
+                temp = new Node(node->getMoveGenerator().MakeAMove(node->getBoard(), move), 0);
+
+
+            float eval = alphaBeta(temp, depth - 1, alpha, beta, false);
+            maxEval = std::max(alpha, eval);
+            alpha = std::max(alpha, maxEval);
+            if(alpha >= beta) {
+                delete temp;
+                return maxEval;
+            }
+        }
+        delete temp;
+        return maxEval;
+    } else {
+        Node *temp;
+        float minEval = INFINITY;
+        for(auto &move : node->getMoveGenerator().getMoves()) {
+            if(move.isItCastling())
+                temp = new Node(node->getMoveGenerator().MakeCastlingMove(node->getBoard(), move), 0);
+            else
+                temp = new Node(node->getMoveGenerator().MakeAMove(node->getBoard(), move), 0);
+
+            float eval = alphaBeta(temp, depth - 1, alpha, beta, true);
+            minEval = std::min(beta, eval);
+            beta = std::min(beta, minEval);
+            if(beta <= alpha) {
+                delete temp;
+                return minEval;
+            }
+        }
+        delete temp;
+        return minEval;
     }
 }
 
